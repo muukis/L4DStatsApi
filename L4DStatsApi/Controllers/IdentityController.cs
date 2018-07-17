c44fcc37f4a5bfef831713268e5ad0f2585bd919
Mikko Andersson <mikko.andersson@vincit.fi>
2018-07-17 14:45:18 +0300

    Removed unnecessary debug API method

--- L4DStatsApi/Controllers/IdentityController.cs
+++ L4DStatsApi/Controllers/IdentityController.cs
@@ -25,51 +25,6 @@ namespace L4DStatsApi.Controllers
             this.service = service;
         }
 
-#if DEBUG
-        /// <summary>
-        /// Create TESTING identity token
-        /// </summary>
-        /// <returns><see cref="GameSeverIdentityResult"/> object</returns>
-        [HttpGet]
-        [SwaggerOperation("CreateTestingBearerToken")]
-        [SwaggerResponse(200, typeof(GameSeverIdentityResult), "Returns a game server testing identity token for the stats API.")]
-        [SwaggerResponse(400, typeof(ErrorResult), "Invalid request")]
-        [SwaggerResponse(500, typeof(ErrorResult), "Internal server error")]
-        [SwaggerResponse(900, typeof(ErrorResult), "Invalid login")]
-        public async Task<IActionResult> CreateTestingBearerToken()
-        {
-            try
-            {
-                var token = await service.CreateGameServerIdentityToken(new LoginBody
-                {
-                    GameServerGroupPrivateKey = Guid.Parse("66edfde5-54d6-4a4d-91b6-40209eb9414c"),
-                    GameServerPrivateKey = Guid.Parse("4b12123c-896c-4e01-b966-a2cf57b63357")
-                });
-
-                if (token == null)
-                {
-                    return Error(new ErrorResult
-                    {
-                        Code = 900,
-                        Classification = ErrorClassification.EntityNotFound,
-                        Message = "Invalid login information"
-                    }, HttpStatusCode.NotFound);
-                }
-
-                return Ok(token);
-            }
-            catch (Exception)
-            {
-                return Error(new ErrorResult
-                {
-                    Code = 500,
-                    Classification = ErrorClassification.InternalError,
-                    Message = "Failed creating identity token"
-                });
-            }
-        }
-#endif
-
         /// <summary>
         /// Create identity token
         /// </summary>
